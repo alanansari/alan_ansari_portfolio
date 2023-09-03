fe7c4c97b1842a219d9e3bb8cbf1f10e795cac51
alan-mathongo <alan.ansari@mathongo.com>
2023-09-03 12:26:06 +0530

    CSS animations

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 const box = document.getElementById('box');
 const body = document.querySelector('body');
-const root = document.documentElement;
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-    card.onmousemove = e => {
-        root.style.setProperty('--rad', '16rem');
-    }
-    card.onmouseleave = e => {
-        root.style.setProperty('--rad', '10rem');
-    }
-});
- 
-document.onmousemove = evt => {
-    let x = (evt.clientX);
-    let y = (evt.clientY);
- 
-    root.style.setProperty('--x', x+'px');
-    root.style.setProperty('--y', y+'px');
-};
 
 let string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*()^%$@!~`{}[]|\/?.,><;:+=-_';
 
@@ -29,7 +10,7 @@ function randomChar() {
 
 function randomSrting() {
     let string = '';
-    for(let i = 0; i < 6000; i++) {
+    for(let i = 0; i < 10000; i++) {
         string += randomChar();
     }
     return string;
